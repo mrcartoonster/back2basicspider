70883a9adcc257e140287efdb2b1fdc821750c8f
Mrnobody <mrcartoonster@icloud.com>
2020-05-23 01:00:56 +0000

    📦 NEW: SCRAP-13 #1h parsing the xpaths

--- back/basics/spiders/base.py
+++ back/basics/spiders/base.py
@@ -4,7 +4,6 @@ import scrapy
 
 class BaseSpider(scrapy.Spider):
     name = 'base'
-    allowed_domains = ['https://lifehacker.com/c/back-to-basics']
     start_urls = ['http://https://lifehacker.com/c/back-to-basics/']
 
     def parse(self, response):
