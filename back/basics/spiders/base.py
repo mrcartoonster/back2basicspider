8904a1b495187822d8e2917e3a731b5e7fc14715
Mrnobody <mrcartoonster@icloud.com>
2020-05-27 18:22:48 +0000

    SCRAP-13 adding pre-commits

--- back/basics/spiders/base.py
+++ back/basics/spiders/base.py
@@ -3,8 +3,8 @@ import scrapy
 
 
 class BaseSpider(scrapy.Spider):
-    name = 'base'
-    start_urls = ['http://https://lifehacker.com/c/back-to-basics/']
+    name = "base"
+    start_urls = ["http://https://lifehacker.com/c/back-to-basics/"]
 
     def parse(self, response):
         pass
