9b5054915051c6a62e0e9ac0c8a981d9f34cf6dc
Mrnobody <mrcartoonster@icloud.com>
2020-05-29 05:30:41 +0000

    📦 NEW: SCRAP-13 #time 30m creating spider

--- back/basics/spiders/base.py
+++ back/basics/spiders/base.py
@@ -4,10 +4,27 @@ import scrapy
 
 class BaseSpider(scrapy.Spider):
     name = "base"
-    start_urls = ["http://https://lifehacker.com/c/back-to-basics/"]
+    start_urls = ["https://lifehacker.com/c/back-to-basics/"]
 
     def parse(self, response):
         """
         Parser for traversing the back-to-basics site.
         """
-        pass
+        articles = response.xpath('//div[@class="cw4lnv-5 aoiLP"]')
+        for href in articles:
+            yield response.follow(href, callback=self.parsing)
+
+        page = back = response.xpath('//div[@class="qsfpej-0 bIkJGf"]')
+        for href in page:
+            yield response.follow(herf, callback=self.parse)
+
+    def parsing(self, response):
+        """
+        Parsing first article from within posts.
+        """
+        arts = response.xpath(
+            "/html/body/div[3]/div[5]/main/div/div[2]/div[2]"
+        )
+
+        for art in arts:
+            yield {art.xpath("./p[1]/text()").get()}
