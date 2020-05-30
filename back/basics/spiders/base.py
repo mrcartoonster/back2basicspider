9a8e2b6696dacd0aa62aa6e7ea4adf57390f0e12
Mrnobody <mrcartoonster@icloud.com>
2020-05-30 07:08:53 +0000

    👌 IMPROVE: SCRAP-13 #time 30m scraping title and first paragraph.

--- back/basics/spiders/base.py
+++ back/basics/spiders/base.py
@@ -10,8 +10,22 @@ class BaseSpider(scrapy.Spider):
         """
         Parser for traversing the back-to-basics site.
         """
-        articles = response.xpath('//div[@class="cw4lnv-5 aoiLP"]')
-        for a in articles:
+        articles = response.xpath('//div[@class="cw4lnv-5 aoiLP"]/a')
+        for href in articles:
+            yield response.follow(href, callback=self.parsing)
+
+    def parsing(self, response):
+        """
+        Parsing first paragraph.
+        """
+        para = response.xpath(
+            '//div[@class="r43lxo-0 hEDDLA js_post-content"]'
+        )
+
+        for p in para:
             yield {
-                'h': a.xpath('./a/@href').get()
+                "para": p.xpath("./p[1]/text()").get(),
+                "title": p.xpath(
+                    '//header[@class="sc-1efpnfq-1 hQgdUv"]/h1/a/text()'
+                ),
             }
