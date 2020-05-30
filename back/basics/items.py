64346ef84864da561336f297940b3706c1a6d63b
Mrnobody <mrcartoonster@icloud.com>
2020-05-30 08:45:25 +0000

    📦 NEW: SCRAP-13 #time 30m changed to loaders!

--- back/basics/items.py
+++ back/basics/items.py
@@ -6,9 +6,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
 
 class BasicsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst()
+    )
+    para = scrapy.Field(
+        input_processor=MapCompose(remove_tags), output_processor=Join()
+    )
--- back/basics/spiders/base.py
+++ back/basics/spiders/base.py
@@ -1,31 +1,39 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from basics.items import BasicsItem
+from scrapy.loader import ItemLoader
 
 
 class BaseSpider(scrapy.Spider):
+    """Prime spider for for getting titles and first paragraph."""
+
     name = "base"
     start_urls = ["https://lifehacker.com/c/back-to-basics/"]
 
     def parse(self, response):
-        """
-        Parser for traversing the back-to-basics site.
-        """
+        """Parser for traversing the back-to-basics site."""
         articles = response.xpath('//div[@class="cw4lnv-5 aoiLP"]/a')
         for href in articles:
             yield response.follow(href, callback=self.parsing)
 
     def parsing(self, response):
-        """
-        Parsing first paragraph.
-        """
+        """Parse first paragraph."""
         para = response.xpath(
             '//div[@class="r43lxo-0 hEDDLA js_post-content"]'
         )
 
         for p in para:
-            yield {
-                "para": p.xpath("./p[1]/text()").get(),
-                "title": p.xpath(
-                    '//header[@class="sc-1efpnfq-1 hQgdUv"]/h1/a/text()'
-                ).get(),
-            }
+            loader = ItemLoader(item=BasicsItem(), selector=p)
+            loader.add_xpath(
+                "title",
+                '//header[@class="sc-1efpnfq-1 hQgdUv"]/h1/a/text() | //h1[@class="gkv9lo-4 eNOFiQ"]/a/text() | //a[@class="sc-1out364-0 hMndXN js_link"]/h1/text()',
+            )
+            loader.add_xpath("para", "./p[1]/text()")
+            yield loader.load_item()
+        #   for p in para:
+        #       yield {
+        #           "para": p.xpath("./p[1]/text()").get(),
+        #           "title": p.xpath(
+        #               '//header[@class="sc-1efpnfq-1 hQgdUv"]/h1/a/text() | //h1[@class="gkv9lo-4 eNOFiQ"]/a/text() | //a[@class="sc-1out364-0 hMndXN js_link"]/h1/text()'
+        #           ).get(),
+        #       }
