ebbfad3ca4bf22f387c254b036497bde146f6254
Mrnobody <mrcartoonster@icloud.com>
2020-06-02 18:31:30 +0000

    📦 NEW: SCRAP-13 running pre-commit to clean up all files

--- back/basics/models.py
+++ back/basics/models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import create_engine, Column, MetaData, Table
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, MetaData, Table, create_engine
 from sqlalchemy.dialects.postgresql import TEXT
-
 
 metadata = MetaData()
 
 basics = Table(
-    'basics',
+    "basics",
     metadata,
-    Column('title', TEXT),
-    Column('author', TEXT),
-    Column('para', TEXT),
+    Column("title", TEXT),
+    Column("author", TEXT),
+    Column("para", TEXT),
 )
--- back/basics/pipelines.py
+++ back/basics/pipelines.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from databases import Database
-from scrapy.exceptions import DropItem
 from models import basics
+from scrapy.exceptions import DropItem
 
 
 class BasicsPipeline:
     """Pipeline to drop authors that are not Beth Skwarecki or Claire Lower."""
 
-    def __init__(self, database):
-        pass
+    # Uncomment and integrate once your get database insertion working!
+    #   def process_item(self, item, spider):
+    #       """Check to see if author is in our list."""
+    #       if item.get("author") in ["Beth Skwarecki", "Claire Lower"]:
+    #           return item
 
+    #       else:
+    #           raise DropItem("Not the authors we want.")
 
     def process_item(self, item, spider):
-        """Check to see if author is in our list."""
-        if item.get("author") in ["Beth Skwarecki", "Claire Lower"]:
-            return item
-
-        else:
-            raise DropItem("Not the authors we want.")
+        """Database pipeline insertion with Encode databases."""
+        pass
