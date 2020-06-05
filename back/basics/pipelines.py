88ba6b0b2c6e69a040a3cf1962569ef28b2748e9
Mrnobody <mrcartoonster@icloud.com>
2020-06-05 21:27:17 +0000

    👌 IMPROVE: Adding drop item pipeline

--- back/basics/pipelines.py
+++ back/basics/pipelines.py
@@ -1,53 +1,14 @@
 # -*- coding: utf-8 -*-
-from databases import Database
-
-from .models import basics
-from .settings import DB_URI
+from scrapy.exceptions import DropItem
 
 
 class BasicsPipeline:
-    """
-    Pipeline to drop authors that are not Beth Skwarecki or Claire Lower as
-    well as inserting authors name, article title and first paragrapg into
-    Postgres database.
-    """
+    """Pipeline to drop authors that are not Beth Skwarecki or Claire Lower."""
 
-    def __init__(self, db_uri):
+    def process_item(self, item, spider):
+        """Check to see if author is in our list."""
+        if item.get("author") in ["Beth Skwarecki", "Claire Lower"]:
+            return item
 
-        """
-        Will change this using dataclasses once inserting items into Postgres
-        database is successful.
-        """
-        self.db_uri = db_uri
-        self.db = db
-
-    @classmethod
-    def from_crawler(cls, crawler):
-
-        """
-         Scrapy crawler object to retreive database URL and database
-         model from settings.py.
-         """
-        return cls(
-            db_uri=crawler.settings.get("DB_URI"),
-            db=crawler.settingsg.get("DATABASE"),
-        )
-
-    async def open_spider(self, spider):
-
-        """Asyncio database connection."""
-        self.db = Database(self.db_uri)
-        await self.db.connect()
-
-    async def close_spider(self, spider):
-
-        """
-        Asyncio database disconnect. Will create an async context block once
-        able to insert items into Postgres database.
-        """
-        await self.db.disconnect()
-
-    async def process_item(self, item, spider):
-        query = basics.insert()
-        await database.execute(query=query, values=item)
-        return item
+        else:
+            raise DropItem("Not the authors we want.")
