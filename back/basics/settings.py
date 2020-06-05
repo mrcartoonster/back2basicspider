07f0c46ad42b753e621886d01de10ac62506c6eb
Mrnobody <mrcartoonster@icloud.com>
2020-06-05 21:39:54 +0000

    👌 IMPROVE: SCRAP-14 finished tutorial basics

--- back/basics/settings.py
+++ back/basics/settings.py
@@ -82,7 +82,7 @@ ITEM_PIPELINES = {
 # each remote server
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUe = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
