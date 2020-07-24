f4623f44d99e2388a2d2f77af4832fd01f3cd7d8
Evan Baird <mrcartoonster@gmail.com>
2020-07-24 16:09:50 -0600

    📦 NEW: Refactoring, updated to Scrapy 2.2.1 and adding ItemAdapter class to pipelines

--- back/basics/pipelines.py
+++ back/basics/pipelines.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
+from dataclasses import dataclass
+
+from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 
 
+@dataclass
 class BasicsPipeline:
     """Pipeline to drop authors that are not Beth Skwarecki or Claire Lower."""
 
+    ids_seen = set()
+
     def process_item(self, item, spider):
         """Check to see if author is in our list."""
-        if item.get("author") in ["Beth Skwarecki", "Claire Lower"]:
+        adapter = ItemAdapter
+        if adapter.get("author") in ["Beth Skwarecki", "Claire Lower"]:
+            self.ids_seen.add(adapter.get("author"))
             return item
-
         else:
             raise DropItem("Not the author we want.")
